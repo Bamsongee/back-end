872303f6450b1bc54b7b653fccd6e8a820859ad9
7beunseo <kimes0403@gmail.com>
2024-07-09 18:36:01 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/ohmea/todayrecipe/TodayrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.ohmea.todayrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayrecipeApplication.class, args);
+	}
+
+}
