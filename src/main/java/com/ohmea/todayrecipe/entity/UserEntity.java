2f5d9aa5e9670e1830f48df69ab966bccc4cb2c6
7beunseo <kimes0403@gmail.com>
2024-07-11 03:15:18 +0900

    Feat: UserEntity 작성

--- src/main/java/com/ohmea/todayrecipe/entity/UserEntity.java
+++ src/main/java/com/ohmea/todayrecipe/entity/UserEntity.java
@@ -11,22 +11,12 @@ import lombok.NoArgsConstructor;
 public class UserEntity extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+    @Column(name = "user_id")
     private Long id;
 
     private String username;
 
-    private String email;
-
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Authority authority;
-
-    @Builder
-    public UserEntity(String email, String password, Authority authority) {
-        this.email = email;
-        this.password = password;
-        this.authority = authority;
-    }
+    private String role;
 }
