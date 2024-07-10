16401efab35a9ffdf8b04e721e8c1f122018820c
7beunseo <kimes0403@gmail.com>
2024-07-11 03:12:22 +0900

    Feat: Security Config 클래스 작성

--- src/main/java/com/ohmea/todayrecipe/config/SecurityConfig.java
+++ src/main/java/com/ohmea/todayrecipe/config/SecurityConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    // 암호화에 사용
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -22,6 +25,19 @@ public class SecurityConfig {
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+
+                // 경로별 인가 작업
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/register").permitAll()
+                        .requestMatchers("/reissue").permitAll()
+                        .anyRequest().authenticated()) // 로그인한 사용자만 접근
+
+                // 세션 설정
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+
                 .build();
     }
 
