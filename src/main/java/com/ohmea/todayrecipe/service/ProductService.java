1f8bab770e841cb0dc877d8c8cfc0f227975ebfa
7beunseo <kimes0403@gmail.com>
2024-07-21 08:26:19 +0900

    Feat: 할인 상품 데이터베이스 초기 데이터 설정 #14

--- src/main/java/com/ohmea/todayrecipe/service/ProductService.java
+++ src/main/java/com/ohmea/todayrecipe/service/ProductService.java
@@ -4,6 +4,8 @@ import com.ohmea.todayrecipe.dto.product.ProductResponseDTO;
 import com.ohmea.todayrecipe.dto.response.ResponseDTO;
 import com.ohmea.todayrecipe.entity.ProductEntity;
 import com.ohmea.todayrecipe.repository.ProductRepository;
+import com.ohmea.todayrecipe.util.CsvReader;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductService {
     @Autowired private final ProductRepository productRepository;
+    @Autowired private CsvReader csvReader;
+    private static final String CSV_FILE_PATH = "static/product_entity.csv";
+
+    @PostConstruct
+    public void init() {
+        List<ProductEntity> products = csvReader.readProductsFromCsv(CSV_FILE_PATH);
+        productRepository.saveAll(products);
+    }
 
     /**
      * 할인 상품 조회
