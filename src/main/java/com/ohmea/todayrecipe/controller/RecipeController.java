5f6470df92c8c54376833f0c01dd8f380f9d30f3
gyurili <inglifestora@naver.com>
2024-07-22 00:44:48 +0900

    레시피 검색 2

--- src/main/java/com/ohmea/todayrecipe/controller/RecipeController.java
+++ src/main/java/com/ohmea/todayrecipe/controller/RecipeController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam; // 이 줄을 추가하세요
+import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
