cdda66b57c90f738d0e89e8c28a7d5fc030ea6a8
gyurili <inglifestora@naver.com>
2024-08-18 00:39:55 +0900

    cors 추가 수정

--- src/main/java/com/ohmea/todayrecipe/config/CorsConfig.java
+++ src/main/java/com/ohmea/todayrecipe/config/CorsConfig.java
@@ -9,10 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .exposedHeaders("accesstoken", "refreshtoken")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
